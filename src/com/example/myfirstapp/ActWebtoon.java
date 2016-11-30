6223dc720dc50554d3201241e1b5e86c968930b3
Sung Wook <squirrelswj@gmail.com>
2016-11-30 20:18:08 +0900

    fourth commit

--- src/com/example/myfirstapp/ActWebtoon.java
+++ src/com/example/myfirstapp/ActWebtoon.java
@@ -3,12 +3,15 @@ package com.example.myfirstapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
-public class ActWebtoon extends Activity {
+public class ActWebtoon extends Activity implements OnClickListener {
 
 	Intent getIntent;
-	TextView title;
+	TextView title,btn_naver,btn_daum,btn_nate,btn_lezhin;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +25,56 @@ public class ActWebtoon extends Activity {
 
 	}
 
+
+	/**
+	 * 레이아웃을 지정하기 위한 함수 입니다.
+	 */
 	private void setLayout(){
 		getIntent = getIntent();
 
 		title = (TextView) findViewById(R.id.title);
+		btn_naver = (TextView) findViewById(R.id.btn_naver);
+		btn_daum = (TextView) findViewById(R.id.btn_daum);
+		btn_lezhin = (TextView) findViewById(R.id.btn_Lezhin);
+		btn_nate = (TextView) findViewById(R.id.btn_nate);
+
+		btn_naver.setOnClickListener(this);
+		btn_daum.setOnClickListener(this);
+		btn_lezhin.setOnClickListener(this);
+		btn_nate.setOnClickListener(this);
+
 
 		String v_title = getIntent.getStringExtra("title");
 		int v_subtitle = getIntent.getIntExtra("subtitle", 0);
 
 		title.setText(v_title +", subtitle : "+v_subtitle);
+
+	}
+
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+		switch (v.getId()) {
+		case R.id.btn_naver:
+			
+			break;
+		case R.id.btn_daum:
+
+			break;
+
+		case R.id.btn_Lezhin:
+
+			break;
+
+		case R.id.btn_nate:
+
+			break;
+			
+		default:
+			break;
+		}
 
 	}
 
